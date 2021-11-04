aa76e019b26990b868ebde51209444648e326bc7
i428672 <428672@student.fontys.nl>
2021-11-04 13:35:29 +0100

    Servises class added

---
+++ smartparkingapp/smartparkingapp/src/main/java/com/sioux/smartparkingapp/servises/AppointmentDetailsServise.java
@@ -0,0 +1,44 @@
+package com.sioux.smartparkingapp.servises;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.sioux.smartparkingapp.Repo.AppointmentRepository;
+import com.sioux.smartparkingapp.models.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AppointmentDetailsServise {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public AppointmentDetailsServise(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+
+    public Appointment saveAppointment(Appointment appointment){
+        return appointmentRepository.save(appointment);
+    }
+    public List<Appointment> getAppointments(){
+        return appointmentRepository.findAll();
+    }
+    public Appointment findAppointmentById(Long id){
+        return appointmentRepository.findById(id).orElse(null);
+    }
+    public String deleteAppointment(Long id){
+        appointmentRepository.deleteById(id);
+        return "Appointment removed";
+    }
+    public String deleteAll(){
+        appointmentRepository.deleteAll();
+
+        return "appointment removed";
+    }
+    public Appointment updateAppointment(Appointment appointment){
+        return appointmentRepository.save(appointment);
+    }
+}
