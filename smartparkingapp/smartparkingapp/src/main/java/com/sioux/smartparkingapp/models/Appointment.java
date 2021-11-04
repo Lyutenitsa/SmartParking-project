9833193698be3e376cb3abc0d020ff0585659e18
i428672 <428672@student.fontys.nl>
2021-11-04 12:48:02 +0100

    Created Appointment relationship

--- smartparkingapp/smartparkingapp/src/main/java/com/sioux/smartparkingapp/models/Appointment.java
+++ smartparkingapp/smartparkingapp/src/main/java/com/sioux/smartparkingapp/models/Appointment.java
@@ -1,17 +1,30 @@
 package com.sioux.smartparkingapp.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.Manager;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "Appointment")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long appointment_id;
 
     @Column(name = "starting_time")
     private LocalDateTime starting_time;
@@ -31,6 +44,10 @@ public class Appointment {
     @Column(name = "visitor_phone")
     private String visitorPhone;
 
+@OneToMany(targetEntity = AppointmentManager.class,cascade = CascadeType.ALL)
+@JoinColumn(name="appointment_id",referencedColumnName = "manager_id")
+private List<AppointmentManager> appointmentManagers;
+
     public Appointment(LocalDateTime starting_time, LocalTime duration, String licensePlate, String visitorName, String managerID, String visitorPhone)
     {
         this.starting_time = starting_time;
@@ -41,87 +58,12 @@ public class Appointment {
         this.visitorPhone = visitorPhone;
     }
 
-    public Appointment()
-    {
-
-    }
-
-    //Getter and setters
-    //region
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public LocalDateTime getStarting_time()
-    {
-        return starting_time;
-    }
-
-    public void setStarting_time(LocalDateTime starting_time)
-    {
-        this.starting_time = starting_time;
-    }
-
-    public LocalTime getDuration()
-    {
-        return duration;
-    }
-
-    public void setDuration(LocalTime duration)
-    {
-        this.duration = duration;
-    }
-
-    public String getLicensePlate()
-    {
-        return licensePlate;
-    }
-
-    public void setLicensePlate(String licensePlate)
-    {
-        this.licensePlate = licensePlate;
-    }
-
-    public String getVisitorName()
-    {
-        return visitorName;
-    }
-
-    public void setVisitorName(String visitorName)
-    {
-        this.visitorName = visitorName;
-    }
-
-    public String getManagerID()
-    {
-        return managerID;
-    }
-
-    public void setManagerID(String managerID)
-    {
-        this.managerID = managerID;
-    }
-
-    public String getVisitorPhone()
-    {
-        return visitorPhone;
-    }
-
-    public void setVisitorPhone(String visitorPhone)
-    {
-        this.visitorPhone = visitorPhone;
-    }
-
-
-    //endregion
 
 
     @Override
     public String toString()
     {
-        return "Appointment{" + "id=" + id + ", starting_time=" + starting_time +
+        return "Appointment{" + "id=" + appointment_id + ", starting_time=" + starting_time +
                 ", duration=" + duration + ", licensePlate='" + licensePlate + '\'' +
                 ", visitorName='" + visitorName + '\'' + ", managerID='" + managerID + '\'' +
                 ", visitorPhone='" + visitorPhone + '\'' + '}';
