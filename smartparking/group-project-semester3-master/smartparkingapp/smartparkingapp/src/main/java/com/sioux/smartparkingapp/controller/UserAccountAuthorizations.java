01e84600da6b77c27378c747309e48c3e0f2412f
ISSD <428672@student.fontys.nl>
2021-11-22 18:59:27 +0100

    Done user registraion with default role of user at registration

--- smartparking/group-project-semester3-master/smartparkingapp/smartparkingapp/src/main/java/com/sioux/smartparkingapp/controller/UserAccountAuthorizations.java
+++ smartparking/group-project-semester3-master/smartparkingapp/smartparkingapp/src/main/java/com/sioux/smartparkingapp/controller/UserAccountAuthorizations.java
@@ -74,4 +74,50 @@ public class UserAccountAuthorizations {
     }
 
 
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Username is already taken!"));
+        }
+
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException(" Role is not found."));
+                        roles.add(adminRole);
+
+                                           break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException(" Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("Registration successful"));
+    }
+
+
 }
